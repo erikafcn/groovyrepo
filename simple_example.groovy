145b7582b59a8bdd4e36db34f3545a720b09647b
erikafcn <72034234+erikafcn@users.noreply.github.com>
2023-06-15 13:57:22 +0200

    Create simple_example.groovy
    New simple method example

---
+++ simple_example.groovy
@@ -0,0 +1,25 @@
+/**
+ * This method will print the first not repeating char in the input string or -1 if not present
+ */
+
+def word = 'elephant'
+
+String first_not_repeating_char(String word) {
+    Boolean flag = true
+    for(int i = 0; i < word.size(); i++) {
+        def str = (word - word[i])
+        flag = true
+        for(int j = 0; j < str.size(); j++) {
+            if(word[i] == str[j]) {
+                flag = false
+                break
+            }
+        }
+        if(flag == true) {
+            return word[i]
+
+        }
+    }
+    return -1
+}
+
